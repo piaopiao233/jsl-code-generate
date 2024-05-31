4a66336e2845aab9d0991beacb8f5c5570cbab30
piaopiao <1121264910@qq.com>
2024-05-31 09:54:20 +0800

    修改主方法

--- src/main/java/com/jsl/codegenerate/main/Main.java
+++ src/main/java/com/jsl/codegenerate/main/Main.java
@@ -11,17 +11,23 @@ import com.jsl.codegenerate.model.GenerateConfig;
 public class Main {
 
     public static void main(String[] args) {
+        //数据库配置
         DataSource dsc = new DataSource();
         dsc.setUrl("jdbc:mysql://localhost:3306/iep?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
         dsc.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("pwd");
+        //生成配置
         GenerateConfig generateConfig = new GenerateConfig();
         generateConfig.setDataSource(dsc);
         String projectPath = System.getProperty("user.dir");
+        //代码输出目录
         generateConfig.setOutPutJavaDir(projectPath + "/src/main/java");
+        //包名
         generateConfig.setPackageName("com.jsl.codegenerate");
-        generateConfig.setControllerResultPackagePath("com.jsl.codegenerate.model.DataSource");
+        //Controller层返回结果
+        generateConfig.setControllerResultPackagePath("com.jsl.codegenerate.model.Result");
+        //people需要生成的表名
         AutoGenerator autoGenerator = new AutoGenerator(generateConfig,"people");
         autoGenerator.setDefaultAnalyzes();
         autoGenerator.execute();
---
+++ src/main/java/com/jsl/codegenerate/model/Result.java
@@ -0,0 +1,47 @@
+package com.jsl.codegenerate.model;
+
+import lombok.Data;
+
+/**
+ * @author piaopiao
+ * @description
+ * @date 2021/9/24 13:07
+ */
+@Data
+public class Result {
+    private int code;
+    private String msg;
+    private Object data;
+
+    public static Result succ(Object data) {
+        return succ(200, "操作成功", data);
+    }
+
+    public static Result succ() {
+        return succ(200, "操作成功", null);
+    }
+
+    public static Result succ(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
+    }
+
+    public static Result fail(String msg) {
+        return fail(400, msg, null);
+    }
+
+    public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+
+    public static Result fail(int code, String msg, Object data) {
+        Result r = new Result();
+        r.setCode(code);
+        r.setMsg(msg);
+        r.setData(data);
+        return r;
+    }
+}
